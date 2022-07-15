0661d28af69aeed2f7d40c6692752a41d9e0a1f0
Athira-M-Chandran <athiramchandran90@gmail.com>
2022-07-15 13:28:05 +0530

    configuration

--- flight_fare/config/configuration.py
+++ flight_fare/config/configuration.py
@@ -18,47 +18,108 @@ class Configuartion:
         except Exception as e:
             raise ProjectException(e,sys) from e
 
-def get_data_ingestion_config(self) ->DataIngestionConfig:
+    def get_data_ingestion_config(self) ->DataIngestionConfig:
+            try:
+                artifact_dir = self.training_pipeline_config.artifact_dir
+                data_ingestion_artifact_dir=os.path.join(
+                    artifact_dir,
+                    DATA_INGESTION_ARTIFACT_DIR,
+                    self.time_stamp
+                )
+                data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
+                
+                train_dataset_download_url = data_ingestion_info[DATA_INGESTION_TRAIN_DOWNLOAD_URL_KEY]
+                test_dataset_download_url = data_ingestion_info[DATA_INGESTION_TEST_DOWNLOAD_URL_KEY]
+                sample_dataset_download_url = data_ingestion_info[DATA_INGESTION_SAMPLE_DOWNLOAD_URL_KEY]
+                
+                ingested_data_dir = os.path.join(
+                    data_ingestion_artifact_dir,
+                    data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
+                )
+                ingested_train_dir = os.path.join(
+                    ingested_data_dir,
+                    data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
+                )
+                ingested_test_dir =os.path.join(
+                    ingested_data_dir,
+                    data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
+                )
+                ingested_sample_dir = os.path.join(
+                    ingested_data_dir,
+                    data_ingestion_info[DATA_INGESTION_SAMPLE_DIR_KEY]
+                )
+
+
+                data_ingestion_config=DataIngestionConfig(
+                    train_dataset_download_url=train_dataset_download_url,
+                    test_dataset_download_url = test_dataset_download_url,
+                    sample_dataset_download_url = sample_dataset_download_url,
+                    ingested_train_dir=ingested_train_dir, 
+                    ingested_test_dir=ingested_test_dir,
+                    ingested_sample_dir= ingested_sample_dir
+                )
+                logging.info(f"Data Ingestion config: {data_ingestion_config}")
+                return data_ingestion_config
+            except Exception as e:
+                raise ProjectException(e,sys) from e
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+            try:
+                artifact_dir = self.training_pipeline_config.artifact_dir
+
+                data_validation_artifact_dir=os.path.join(
+                    artifact_dir,
+                    DATA_VALIDATION_ARTIFACT_DIR_NAME,
+                    self.time_stamp
+                )
+                data_validation_config = self.config_info[DATA_VALIDATION_CONFIG_KEY]
+
+
+                schema_file_path = os.path.join(ROOT_DIR,
+                data_validation_config[DATA_VALIDATION_SCHEMA_DIR_KEY],
+                data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY]
+                )
+
+                report_file_path = os.path.join(data_validation_artifact_dir,
+                data_validation_config[DATA_VALIDATION_REPORT_FILE_NAME_KEY]
+                )
+
+                report_page_file_path = os.path.join(data_validation_artifact_dir,
+                data_validation_config[DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY]
+
+                )
+
+                data_validation_config = DataValidationConfig(
+                    schema_file_path=schema_file_path,
+                    report_file_path=report_file_path,
+                    report_page_file_path=report_page_file_path,
+                )
+                return data_validation_config
+            except Exception as e:
+                raise ProjectException(e,sys) from e
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        pass
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        pass
+
+    def get_model_evaluation_config(self) ->ModelEvaluationConfig:
+        pass
+
+    def get_model_pusher_config(self) -> ModelPusherConfig:
+        pass
+
+    def get_training_pipeline_config(self) ->TrainingPipelineConfig:
         try:
-            artifact_dir = self.training_pipeline_config.artifact_dir
-            data_ingestion_artifact_dir=os.path.join(
-                artifact_dir,
-                DATA_INGESTION_ARTIFACT_DIR,
-                self.time_stamp
-            )
-            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
-            
-            train_dataset_download_url = data_ingestion_info[DATA_INGESTION_TRAIN_DOWNLOAD_URL_KEY]
-            test_dataset_download_url = data_ingestion_info[DATA_INGESTION_TEST_DOWNLOAD_URL_KEY]
-            sample_dataset_download_url = data_ingestion_info[DATA_INGESTION_SAMPLE_DOWNLOAD_URL_KEY]
-            
-            ingested_data_dir = os.path.join(
-                data_ingestion_artifact_dir,
-                data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
-            )
-            ingested_train_dir = os.path.join(
-                ingested_data_dir,
-                data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
-            )
-            ingested_test_dir =os.path.join(
-                ingested_data_dir,
-                data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
-            )
-            ingested_sample_dir = os.path.join(
-                ingested_data_dir,
-                data_ingestion_info[DATA_INGESTION_SAMPLE_DIR_KEY]
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(ROOT_DIR,
+            training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY]
             )
 
-
-            data_ingestion_config=DataIngestionConfig(
-                train_dataset_download_url=train_dataset_download_url,
-                test_dataset_download_url = test_dataset_download_url,
-                sample_dataset_download_url = sample_dataset_download_url,
-                ingested_train_dir=ingested_train_dir, 
-                ingested_test_dir=ingested_test_dir,
-                ingested_sample_dir= ingested_sample_dir
-            )
-            logging.info(f"Data Ingestion config: {data_ingestion_config}")
-            return data_ingestion_config
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(f"Training pipleine config: {training_pipeline_config}")
+            return training_pipeline_config
         except Exception as e:
             raise ProjectException(e,sys) from e
